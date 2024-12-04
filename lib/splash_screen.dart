ab98aaa8ecee36b5135aaeb58811e7d0f43b4816
kimsojin <100557009+rlathwls03@users.noreply.github.com>
2024-12-04 17:36:44 +0900

    런처 아이콘 수정 및 시타 변경 사항 반영

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -13,26 +13,41 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   void _navigateToNextScreen() async {
-    await Future.delayed(const Duration(seconds: 1)); // 2초 대기
+    await Future.delayed(const Duration(seconds: 10)); // 2초 대기
     Navigator.pushReplacementNamed(context, '/login'); // 로그인 화면으로 이동
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF1D3557), // 이미지에서 추출한 진한 파란색
+      //backgroundColor: Color(0xFF1D3557), // 진한 파란색 배경
+      backgroundColor: Colors.white,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              'Gen-Z-Calendar',
-              style: TextStyle(
-                fontSize: 36,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
+            // 첫 번째 로고 이미지
+            Image.asset(
+              'assets/calendarlogo.png', // 첫 번째 이미지 경로
+              width: 100, // 원하는 너비
+              height: 100, // 원하는 높이
             ),
+            const SizedBox(height: 20), // 이미지 간격
+            // 두 번째 로고 이미지
+            Image.asset(
+              'assets/Gen_Zlogo.png', // 두 번째 이미지 경로
+              width: 200, // 원하는 너비
+              height: 50, // 원하는 높이
+            ),
+            const SizedBox(height: 20), // 이미지와 텍스트 사이 간격
+            // Text(
+            //   'Gen-Z-Calendar',
+            //   style: TextStyle(
+            //     fontSize: 36,
+            //     fontWeight: FontWeight.bold,
+            //     color: Colors.white,
+            //   ),
+            // ),
             const SizedBox(height: 20),
             CircularProgressIndicator(
               valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
